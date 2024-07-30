2a68cc1c9f5c66d86f44501fd79c0f18fc86fce0
Ayelet Peres <aperes7@gmail.com>
2024-07-29 22:16:16 -0400

    fixed bug for T cell

--- R/functions.R
+++ R/functions.R
@@ -183,7 +183,7 @@ createFullHaplotype <-
           "\\*.*",
           "\\1",
           grep(
-            "^(?=.*IG)",
+            "^(?=.*[IG|TR])",
             unique(unlist(clip_db_sub[clip_db_sub[, hapBy_col] %in% hapBy_alleles, toHap_col], use.names = F)),
             value = T,
             perl = T
